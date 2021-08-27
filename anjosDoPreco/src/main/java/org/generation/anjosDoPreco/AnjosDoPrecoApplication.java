4ec90225387ab2332ad4e34f4215a65e7d2a7d73
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-08-27 10:13:42 -0300

    Projeto Anjos Do Preço - Pt1

---
+++ anjosDoPreco/src/main/java/org/generation/anjosDoPreco/AnjosDoPrecoApplication.java
@@ -0,0 +1,13 @@
+package org.generation.anjosDoPreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjosDoPrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnjosDoPrecoApplication.class, args);
+	}
+
+}
